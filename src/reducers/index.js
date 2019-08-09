10fd61fe4b215c96af214bdf23b3390aa0850e10
Dinesh Ramasamy <dinesh.ramasamy@freshworks.com>
2019-08-09 08:48:16 +0530

    Fix root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import themeReducer from "./theme";
 import counterReducer from "./counter";
 import userReducer from "./user";
 
-const rootReducer = ({ theme, counter }, action) => ({
+const rootReducer = ({ theme, counter, user }, action) => ({
   theme: themeReducer(theme, action),
   counter: counterReducer(counter, action),
-  user: userReducer(counter, action)
+  user: userReducer(user, action)
 });
 
 export default rootReducer;
